824b56f78d0d9e5258e3987ae69c80f9b55e4c15
Bassim Durzi <bassimdurzi@gmail.com>
2023-07-06 15:27:16 +0200

    help user to choose continue using the app or exit it.

--- src/main/java/Workshop/App.java
+++ src/main/java/Workshop/App.java
@@ -6,22 +6,31 @@ public class App {
 
     public static void main(String[] args) {
 
+        System.out.println("Simple Calculator :-)");
+
+        chooseOperation();
+
         Scanner scanner = new Scanner(System.in);
 
+
         while(true)    {
+            System.out.println("Type 1 to continue, or 0 to exit");
+            int isContinue = scanner.nextInt();
+
+            if (isContinue == 0){
+                System.out.println(" ..... Thank you for using my simple calculator :-) .....");
+                System.exit(0);
+            }
 
             System.out.println("Please type the first number:");
             double num1 = scanner.nextDouble();
 
-            System.out.println("Please choose the operation (+ , - , * , / ) or type 'q' to exit the program: ");
+            System.out.println("Please choose the operation: ");
             char operation = scanner.next().charAt(0);
 
             System.out.println("Please type the second number:");
             double num2 = scanner.nextDouble();
 
-            if (operation == 'q'){
-                break;
-            }
 
             switch (operation) {
                 case '+': {
@@ -54,7 +63,17 @@ public class App {
                     break;
             }
             System.out.println("--------------------------------------------------");
+
         }
-        System.out.println(" ..... Thank you for using my simple calculator :-) .....");
+
     }
+    public static void chooseOperation() {
+        System.out.println("Type + for addition");
+        System.out.println("Type - for subtraction");
+        System.out.println("Type * for multiply");
+        System.out.println("Type / for division");
+        System.out.println("Ready to use");
+        System.out.println("^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ^-^ ");
+    }
+
 }
